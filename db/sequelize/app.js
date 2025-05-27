bc40afae4295cb1a287412ca986c20cdf43c00c5
JeonChaeHwan <wjs082290@gmail.com>
2025-05-28 00:38:12 +0900

    됏나

--- db/sequelize/app.js
+++ db/sequelize/app.js
@@ -13,3 +13,30 @@ nunjucks.configure('views', {
   watch: true,
 });
 sequelize.sync({ force: false })
+  .then(() => {
+    console.log('데이터베이스 연결 성공');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+app.unsubscribe(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.unsubscribe(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
+});
+
+app.unsubscribe((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err: {};
+  res.render('error');
+});
+
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기 중');
+})
